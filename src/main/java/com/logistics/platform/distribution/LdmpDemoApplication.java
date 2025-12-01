2b70f6f48e39b8d722f1fa308573cef1bc651933
TT <17766316603@163.com>
2025-12-01 10:18:01 +0800

    初始化项目结构：添加核心代码与配置文件

---
+++ src/main/java/com/logistics/platform/distribution/LdmpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.platform.distribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdmpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdmpDemoApplication.class, args);
+    }
+
+}
